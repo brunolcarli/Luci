f15dd1c505ead1c146d2559e0969f68dd0032031
Bruno L. Carli <brunolcarli@gmail.com>
2020-06-11 19:27:37 -0300

    CHANGE settings url and self id

--- luci/settings.py
+++ luci/settings.py
@@ -7,5 +7,6 @@ __version__ = '0.0.1'
 
 TOKEN = config('TOKEN', '')
 API_URL = config('BOT_API', '')
-LISA_URL = config('LISA', '')
+LISA_URL = config('LISA_URL', '')
 SETTINGS_MODULE = config('SETTINGS_MODULE', 'common')
+SELF_ID = config('SELF_ID', '')
