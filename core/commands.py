63de81c4148fc1ce9bd0efa54d1bef218baef526
Bruno L. Carli <brunolcarli@gmail.com>
2020-06-25 16:41:19 -0300

    ADD quote commands

--- core/commands.py
+++ core/commands.py
@@ -6,8 +6,10 @@ from discord.ext import commands
 from core.output_vectors import (offended, insufficiency_recognition,
                                  propositions, indifference, opinions,
                                  positive_answers, negative_answers)
-from core.utils import validate_text_offense, extract_sentiment, answer_intention
-from luci.settings import __version__, SELF_ID
+from core.external_requests import Query, Mutation
+from core.utils import (validate_text_offense, extract_sentiment, answer_intention,
+                        make_hash, get_gql_client)
+from luci.settings import __version__, SELF_ID, API_URL
 
 
 client = commands.Bot(command_prefix='!')
@@ -105,3 +107,57 @@ async def version(discord):
     Pinga o bot para teste sua execução
     """
     await discord.send(__version__)
+
+
+@client.command(aliases=['rquote', 'rq'])
+async def random_quote(bot):
+    """
+    Retorna um quote aleatório.
+    """
+    server = make_hash(bot.guild.name, bot.guild.id)
+    payload = Query.get_quotes(server.decode('utf-8'))
+    client = get_gql_client(API_URL)
+
+    try:
+        response = client.execute(payload)
+    except Exception as err:
+        print(f'Erro: {str(err)}\n\n')
+        return await bot.send('Erro')
+
+    quotes = response.get('botQuotes')
+    if not quotes:
+        return await bot.send('Ainda não há registros de quotes neste servidor')
+
+    chosen_quote = choice(quotes)
+    embed = discord.Embed(color=0x1E1E1E, type="rich")
+    embed.add_field(name='Quote:', value=chosen_quote, inline=True)
+    return await bot.send('Lembra disso:', embed=embed)
+
+
+@client.command(aliases=['q', 'sq', 'save_quote'])
+async def quote(bot, *args):
+    """
+    Retorna um quote aleatório.
+    """
+    message = ' '.join(word for word in args)
+
+    if not message:
+        return await bot.send(
+            'Por favor insira uma mensagem.\nExemplo:\n'\
+            '``` --quote my name is bond, vagabond ```'
+        )
+
+    server = make_hash(bot.guild.name, bot.guild.id)
+    payload = Mutations.create_quote(message, server.decode('utf-8'))
+    client = get_gql_client(API_URL)
+
+    try:
+        response = client.execute(payload)
+    except Exception as err:
+        print(f'Erro: {str(err)}\n\n')
+        return await bot.send('Erro')
+
+    quote = response.get('botCreateQuote')
+    embed = discord.Embed(color=0x1E1E1E, type="rich")
+    embed.add_field(name='Quote salvo:', value=quote.get('response'), inline=True)
+    return await bot.send('Feito:', embed=embed)
